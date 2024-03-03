e092a0242fb7f5f461722d7b4bf840c183d2ef97
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-03-03 01:17:37 +0100

    Adding BankAccount Class

---
+++ BankAccount/src/BankAccount.java
@@ -0,0 +1,58 @@
+public class BankAccount {
+    private Long id ;
+    private double balance ;
+    private String currency ;
+    private AccountType accountType ;
+    private AccountStatus accountStatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "id=" + id +
+                ", balance=" + balance +
+                ", currency='" + currency + '\'' +
+                ", accountType=" + accountType +
+                ", accountStatus=" + accountStatus +
+                '}';
+    }
+}
