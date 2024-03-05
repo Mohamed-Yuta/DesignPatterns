b4aba180c0c380f31b56a81385112d66ba4c0403
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-03-06 00:24:04 +0100

    test predicate

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -1,15 +1,23 @@
+import model.AccountType;
 import model.BankAccount;
 import repository.AccoutRepositoryImpl;
 import utils.JsonSerializer;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         JsonSerializer<BankAccount> jsonSerializer = new JsonSerializer<>();
         AccoutRepositoryImpl accoutRepository = new AccoutRepositoryImpl();
         accoutRepository.population();
+        List<BankAccount> bankAccountList = accoutRepository.searchAccounts(new Predicate<BankAccount>() {
+            @Override
+            public boolean test(BankAccount bankAccount) {
+                return bankAccount.getAccountType().equals(AccountType.CURRENT);
+            }
+        });
         List<BankAccount> bankAccounts =accoutRepository.findAll();
-        bankAccounts.stream().map(acc->jsonSerializer.toJson(acc)).forEach(System.out::println);
+        bankAccounts.stream().map(jsonSerializer::toJson).forEach(System.out::println);
     }
 }
